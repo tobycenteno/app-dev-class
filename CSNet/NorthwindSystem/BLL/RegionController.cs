806b0a9a20f9a1a73755b55a272238ce591e9d35
tobycenteno <59742161+tobycenteno@users.noreply.github.com>
2020-03-24 09:50:57 -0600

    March24 class

---
+++ CSNet/NorthwindSystem/BLL/RegionController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NorthWindSystem.DAL;
+using NorthWindSystem.Entities;
+
+namespace NorthwindSystem.BLL
+{
+    public class RegionController
+    {
+        public List<Region> Regions_List()
+        {
+
+        }
+
+        public Region Regions_FindByID(int regionid)
+        {
+            using(var context = new NorthwindContext())
+            {
+                return context.Regions.Find(regionid);
+            }
+        }
+
+    }
+
+
+
+}
